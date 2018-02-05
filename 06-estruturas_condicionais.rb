2048601945e7cf302dba9b4ffbf2b7aa664da548
Elisio Filho <elisionfilho@gmail.com>
2018-02-05 15:45:35 -0300

    conditional structures update

--- 06-estruturas_condicionais.rb
+++ 06-estruturas_condicionais.rb
@@ -1,8 +1,37 @@
 puts "Digite um número:"
 v1 = gets.chomp.to_i
 
-if v1 > 10
+# Condicional IF
+if v1 > 10 then
 	puts "O valor digitado é maior que 10"
+elsif v1 >= 5
+	puts "O valor é maior ou igual a 5 (entre 5 e 10)"
 else
-	puts "O valor digitado é menor ou igual 10"
+	puts "O valor digitado é menor ou igual 5"
 end
+
+# A menos que
+=begin
+unless v1 > 10
+	puts "O número digitado é menor que 10"
+else
+	puts "o número digitado é maior que 10"
+end
+=end
+
+=begin
+case v1
+	when 1
+		puts "Você escolheu a opção 1"
+	when 2
+		puts "Você escolheu a opção 2"
+	when 3
+		puts "Você escolheu a opção 3"
+	when 4
+		puts "Você escolheu a opção 4"
+	when 5
+		puts "Você escolheu a opção 5"
+	else
+	  puts "Opção inválida!"
+end
+=end
