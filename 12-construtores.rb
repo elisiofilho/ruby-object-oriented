e8b7550dca48c03b7a045fccfb27fcbcc0f110ef
Elisio Filho <elisionfilho@gmail.com>
2018-02-13 20:39:50 -0300

    builders

---
+++ 12-construtores.rb
@@ -0,0 +1,20 @@
+class Pessoa
+	attr_accessor :nome
+	attr_accessor :idade
+
+	def initialize(nome, idade)
+		@nome = nome
+		@idade = idade
+	end
+end
+
+###########################
+
+pessoa1 = Pessoa.new("Elisio Filho", 20)
+pessoa2 = Pessoa.new("Neto", 25)
+
+puts pessoa1.nome
+puts pessoa1.idade
+
+puts pessoa2.nome
+puts pessoa2.idade
