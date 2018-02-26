867b919c8f5c76246025d229d428af4f14a65d1f
Elisio Filho <elisionfilho@gmail.com>
2018-02-26 11:27:58 -0300

    update loading

--- jogos/adivinhar_numero/lib/loading.rb
+++ jogos/adivinhar_numero/lib/loading.rb
@@ -1,6 +1,18 @@
+require 'tty-cursor'
+require 'tty-spinner'
+
 class Loading
 	def self.load
 		system('clear')
+
+		cursor = TTY::Cursor
+		print cursor.move_to(15, 20)
+
+		spinner = TTY::Spinner.new
+		spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :pulse_2)
+		spinner.auto_spin # Automatic animation with default interval
+		sleep(2) # Perform task
+		spinner.stop('Done!') # Stop animation
 
 		print "Inicializando."
 		4.times do |i|
